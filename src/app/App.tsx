8095361377accc309bf9e45ef6a68dc3b55152d6
liza <missis.liza-m@yandex.ru>
2024-06-13 18:01:17 +0900

    lesson 12 13 Sidebar Navbar + 2 loader для чтения svg

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,22 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
-import { AppRouter } from './providers/router';
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import { Sidebar } from 'widgets/Sidebar';
 
 
 const App = () => {
-   const { theme, toggleTheme } = useTheme();
+   const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <AppRouter/>
+            <Navbar/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
